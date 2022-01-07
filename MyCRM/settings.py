f46ed23ddef28eac87a259b73a78b030c16d441a
AntonDubiaha <96083195+AntonDubiaha@users.noreply.github.com>
2022-01-07 14:00:41 +0200

    sqlite DB

--- MyCRM/settings.py
+++ MyCRM/settings.py
@@ -77,15 +77,10 @@ WSGI_APPLICATION = 'MyCRM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mycrmdb',
-        'USER': 'postgres',
-        'PASSWORD': "123456789",
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
